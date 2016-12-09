5243c259b4f563a2b360a02ef65b6493b5585869
Russ Hatcher <russell.hatcher@outlook.com>
2016-12-09 06:41:03 -0500

    Add project files.

---
+++ XMLReaderProject/Framework/ObservableObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+namespace XMLReaderProject
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+
+        //
+        // Sets the value of the field to the given value and invokes
+        // notification only if teh field has actually changed
+        //
+        protected bool SetField<T>(ref T field, T value, string propertyName)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                NotifyPropertyChanged(propertyName);
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
---
+++ XMLReaderProject/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XMLReaderProject
+{
+    public class Employee: ObservableObject
+    {
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                name = value;
+                SetField(ref name, value, "Name");
+            }
+        }
+
+        string surname;
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            private set
+            {
+                name = value;
+                SetField(ref surname, value, "Surname");
+            }
+        }
+
+        string cellphoneNr;
+        public string CellphoneNr
+        {
+            get
+            {
+                return cellphoneNr;
+            }
+            private set
+            {
+                name = value;
+                SetField(ref cellphoneNr, value, "CellphoneNr");
+            }
+        }
+
+        public Employee(string _name, string _surname, string _cellpnonenr)
+        {
+            name = _name;
+            surname = _surname;
+            cellphoneNr = _cellpnonenr;
+        }
+    }
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Employee emp = new Employee("Jane", "Patterson", "8883339994");
+
+            DataContext = emp;
+
+            InitializeComponent();
+        }
+    }
+}
